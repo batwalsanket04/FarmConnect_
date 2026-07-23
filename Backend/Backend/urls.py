ab78bf1479e02b380e87d65f65f383f7b2b2d6af
sanket batwal <batwalsanket@gmail.com>
2026-07-23 18:09:14 +0530

    Fix root URL

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,33 +1,21 @@
-"""
-URL configuration for Backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from logging import root
 from django.contrib import admin
-from django.urls import path,include
+from django.http import HttpResponse
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('', root),   
-    path('admin/', admin.site.urls),
-    path('api/',include('farmconnect.urls')),
-]
 
+def root(request):
+    return HttpResponse("FarmConnect Backend Running")
+
+
+urlpatterns = [
+    path("", root),
+    path("admin/", admin.site.urls),
+    path("api/", include("farmconnect.urls")),
+]
 
 urlpatterns += static(
     settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
+    document_root=settings.MEDIA_ROOT,
 )
